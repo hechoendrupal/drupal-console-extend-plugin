73e9f6e2d71e4b0790fe973b659776416f97afb9
Matthew Witherow <matthew.witherow@nbcuni.com>
2020-11-06 15:46:53 -0500

    Add the necessary stub methods for composer v2 compatibility

--- src/Extender.php
+++ src/Extender.php
@@ -160,4 +160,20 @@ class Extender implements PluginInterface, EventSubscriberInterface
             $this->io->write('<info>Cache file can not be deleted</info>');
         }
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+      // Nothing to deactivate.
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+      // Nothing to uninstall.
+    }
 }
